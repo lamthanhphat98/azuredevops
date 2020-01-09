d6af5cd96124f8dbfebfce887e23c2664dd2370d
Lam Thanh Phat <lamthanhphat98@gmail.com>
2020-01-09 20:15:32 +0700

    commit all

--- AzureDevops/Controllers/HomeController.cs
+++ AzureDevops/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +16,25 @@ namespace AzureDevops.Controllers
         public IActionResult Get()
         {
             return Ok("ok");
+        }
+        [HttpGet("ip")]
+        public async Task<IActionResult> GetIp()
+        {
+            string responseContent = "";
+            String url = "https://ipinfo.io/ip";
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+
+                    using (HttpContent content = response.Content)
+                    {
+                        responseContent = await content.ReadAsStringAsync();
+                    }
+                }
+            }
+            return Ok(responseContent);
+           
         }
     }
 }
