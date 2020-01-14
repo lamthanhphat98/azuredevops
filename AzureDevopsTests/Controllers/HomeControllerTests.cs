ff0538de5e7386ab8a9e117531e75c78bf1f6196
Lam Thanh Phat <lamthanhphat98@gmail.com>
2020-01-14 21:42:38 +0700

    Unit test

---
+++ AzureDevopsTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AzureDevops.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void GetIpTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ AzureDevopsTests1/Controllers/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AzureDevops.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AzureDevops.Models;
+using System.Linq;
+
+namespace AzureDevops.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        WeatherContext context = new WeatherContext();
+    
+        [TestMethod()]
+        public void GetIpTest()
+        {
+            var getWeather = context.Weather.Where(w => w.Name.Equals("Bangkok")).FirstOrDefault();
+            Assert.AreEqual("Bangkok", getWeather.Name);
+            
+        }
+    }
+}
