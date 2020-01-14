878f7bd7d01e13820e5a75bd4a6408a672061e19
Lam Thanh Phat <lamthanhphat98@gmail.com>
2020-01-14 22:23:36 +0700

    commit test

---
+++ Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AzureDevops.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureDevops.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
