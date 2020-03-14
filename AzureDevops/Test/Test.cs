ff12ef194dc950359573b065d962ea75186034e7
Lam Thanh Phat <lamthanhphat98@gmail.com>
2020-03-14 11:52:31 +0700

    commit 1

--- AzureDevops/Test/Test.cs
+++ AzureDevops/Test/Test.cs
@@ -9,6 +9,6 @@ namespace AzureDevops.Test
     {
         public int a { get; set; }
         public int b { get; set; }
-        public int c { get; set; }
+        public int f { get; set; }
     }
 }
